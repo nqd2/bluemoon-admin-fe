8b4e3d55e6b8c11e224dcab6544e400fe7417108
nqd2 <nguyenquyduc2.00@gmail.com>
2026-01-07 16:26:38 +0700

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export function formatDateTime(dateString: string) {
       const minutes = date.getMinutes().toString().padStart(2, '0');
       const day = date.getDate().toString().padStart(2, '0');
       const month = (date.getMonth() + 1).toString().padStart(2, '0');
-      const year = date.getFullYear().toString().slice(-2);
+      const year = date.getFullYear().toString().slice(-2); 
 
       return `${hours}:${minutes} ${day}/${month}/${year}`;
   } catch (e) {
